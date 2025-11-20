8f603847168d6b883925cd59b3cb174014848a8c
Namit Malasi <namitmalasi@gmail.com>
2025-11-20 16:46:48 +0530

    adding signing up functionality to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@ dotenv.config();
 
 const app = express();
 
-app.use("/api/auth", authRoutes);
-
 const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
