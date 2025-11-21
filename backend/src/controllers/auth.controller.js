f6d88ec5eabbf4cda56f430f848c80f7ddf2d772
Namit Malasi <namitmalasi@gmail.com>
2025-11-21 19:32:23 +0530

    adding routes and tailwind config to frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
-import { generateToken } from "../utils/generateToken.js";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
