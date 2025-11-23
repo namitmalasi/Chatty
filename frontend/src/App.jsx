4858a119b6d555967b417d81ebdea1ca63700812
Namit Malasi <namitmalasi@gmail.com>
2025-11-23 19:52:25 +0530

    adding settings page in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,10 @@ import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore.js";
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -26,7 +28,7 @@ function App() {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
