db056599180625dc14c50f5aad0e25d6e13a4e0c
Namit Malasi <namitmalasi@gmail.com>
2025-11-24 13:41:42 +0530

    fetching users and messages functions

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const response = await axiosInstance.get("/messages/users");
+      set({ users: response.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to load users");
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+    getMessages: async (userId) => {    
+    set({ isMessagesLoading: true });   
+    try {
+      const response = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: response.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to load messages");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+}));
