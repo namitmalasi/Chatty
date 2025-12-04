9eb1d9535cc8ce3a8f169996fbcdbbfc91a9863c
Namit Malasi <namitmalasi@gmail.com>
2025-12-04 18:37:31 +0530

    minor changes

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -53,6 +53,10 @@ export const useChatStore = create((set, get) => ({
     const socket = useAuthStore.getState().socket;
 
     socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
       set({ messages: [...get().messages, newMessage] });
     });
   },
@@ -60,6 +64,6 @@ export const useChatStore = create((set, get) => ({
   unsubscribeFromMessages: () => {
     const socket = useAuthStore.getState().socket;
     socket.off("newMessage");
-  }, 
+  },
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
