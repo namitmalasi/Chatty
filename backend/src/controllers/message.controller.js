8ec2b285540013e3177e298d2448f748e36af30d
Namit Malasi <namitmalasi@gmail.com>
2025-12-04 13:21:09 +0530

    sending messages in real time

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,13 +2,13 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 
 import cloudinary from "../lib/cloudinary.js";
-// import { getReceiverSocketId, io } from "../lib/socket.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({
-       _id: { $ne: loggedInUserId },
+      _id: { $ne: loggedInUserId },
     }).select("-password");
 
     res.status(200).json(filteredUsers);
@@ -51,7 +51,7 @@ export const sendMessage = async (req, res) => {
     }
 
     const newMessage = new Message({
-      senderId, 
+      senderId,
       receiverId,
       text,
       image: imageUrl,
@@ -59,10 +59,10 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    // const receiverSocketId = getReceiverSocketId(receiverId);
-    // if (receiverSocketId) {
-    //   io.to(receiverSocketId).emit("newMessage", newMessage);
-    // }
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios.js";
+import { useAuthStore } from "./useAuthStore.js";
 
 export const useChatStore = create((set, get) => ({
   messages: [],
@@ -45,5 +46,20 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return null;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      set({ messages: [...get().messages, newMessage] });
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
+  }, 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
